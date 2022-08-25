e1a1c105125b811915914d279fa6b20de25b41f4
wirekang <wirekang@gmail.com>
2022-08-25 16:37:18 +0900

    fix

--- docs/set-image.js
+++ docs/set-image.js
@@ -1,15 +1,18 @@
 function init() {
-  const mainImg = document.getElementById("main-img");
-  setInterval(() => {
-    const width = window.innerWidth ?? window.document.documentElement.clientWidth;
-    if (width > 720) {
-      mainImg.className += " desktop";
-      mainImg.src = "images/main-desktop.png";
-    } else {
-      mainImg.className += " mobile";
-      mainImg.src = "images/main-mobile.png";
-    }
-  }, 1000);
+  setImage();
+  setInterval(setImage, 1000);
+}
+
+function setImage() {
+  var mainImg = document.getElementById("main-img");
+  var width = window.innerWidth ?? window.document.documentElement.clientWidth;
+  if (width > 720) {
+    mainImg.className += " desktop";
+    mainImg.src = "images/main-desktop.png";
+  } else {
+    mainImg.className += " mobile";
+    mainImg.src = "images/main-mobile.png";
+  }
 }
 
 window.addEventListener("DOMContentLoaded", init);
