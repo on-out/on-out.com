afea22803d3099d8190afb670ea75116504c0f4f
doyoonkim12345 <kdyem0628@gmail.com>
2023-12-12 20:53:40 +0900

    fix : change image routes

--- docs/js/OnoutSection1.js
+++ docs/js/OnoutSection1.js
@@ -5,8 +5,8 @@ class onoutSection1 extends HTMLElement {
 
         this.innerHTML = `<div class='w-full pt-[5rem] sm:pb-[5rem] bg-[#060606] flex items-center justify-center'>
                             <picture class='max-w-[60rem] w-full h-full flex justify-center items-center px-[1.25rem] sm:px-[3rem]'>
-                                <source srcset='/docs/images/sections/mobile/section1.png' media="(max-width: 640px)" />
-                                <img src='/docs/images/sections/pc/section1.png' />
+                                <source srcset='../images/sections/mobile/section1.png' media="(max-width: 640px)" />
+                                <img src='../images/sections/pc/section1.png' />
                             </picture>
                         <div>`
     }
