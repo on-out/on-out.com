c3b89c1cc709b6ab132a242a504110b09ff1593b
doyoonkim12345 <kdyem0628@gmail.com>
2023-12-12 20:42:36 +0900

    fix : change routes

--- docs/js/OnoutHeader.js
+++ docs/js/OnoutHeader.js
@@ -1,6 +1,6 @@
 class onoutHeader extends HTMLElement {
     connectedCallback(){
-        this.innerHTML = `<div class='w-full flex justify-center bg-black'><div class='max-w-[90rem] w-full py-[1.25rem] px-[1.25rem] sm:px-[3rem] flex justify-start items-center gap-[0.25rem]'><img class='h-[1.5rem] object-center' src='/docs/icons/LOGO.svg' /><span class='text-[#9D9D9D] text-[1rem]'>fashion app</div></div>`
+        this.innerHTML = `<div class='w-full flex justify-center bg-black'><div class='max-w-[90rem] w-full py-[1.25rem] px-[1.25rem] sm:px-[3rem] flex justify-start items-center gap-[0.25rem]'><img class='h-[1.5rem] object-center' src='../icons/LOGO.svg' /><span class='text-[#9D9D9D] text-[1rem]'>fashion app</div></div>`
     }
 }
 
--- docs/js/OnoutSection2.js
+++ docs/js/OnoutSection2.js
@@ -4,17 +4,17 @@ class onoutSection2 extends HTMLElement {
         
         <div class='max-w-[52.5rem] w-full h-full flex justify-center items-center'>
             <picture class='max-w-[60rem] w-full h-full flex justify-center items-center px-[1.25rem] sm:px-[3rem]'>
-                <source srcset='/docs/images/sections/mobile/section2.png' media="(max-width: 640px)" />
-                <img src='/docs/images/sections/pc/section2.png' />
+                <source srcset='../images/sections/mobile/section2.png' media="(max-width: 640px)" />
+                <img src='../images/sections/pc/section2.png' />
             </picture>    
         </div>
         <div class='max-w-[60rem] w-full bg-black py-[3rem] flex flex-col gap-[1rem]'><p class='text-white text-[1rem] leading-[1.5rem] font-bold text-center'>앱다운로드 하기</p>
         <div class='flex items-center w-full justify-center flex gap-[0.77rem]'>
         <a href='https://play.google.com/store/apps/details?id=com.onout.app' target='_blank'>
-        <img src='/docs/icons/store/google.svg' class='w-[7.38rem] sm:w-[11.5rem]'  />
+        <img src='../icons/store/google.svg' class='w-[7.38rem] sm:w-[11.5rem]'  />
         </a>
         <a href='https://apps.apple.com/app/id6446235118' target='_blank'>
-        <img src='/docs/icons/store/apple.svg'  class='w-[7.38rem] sm:w-[11.5rem]'' />
+        <img src='../icons/store/apple.svg'  class='w-[7.38rem] sm:w-[11.5rem]'' />
         </a>
         </div>
         </div>`
